ee61ac555d08f704ee3193052523f51562b2b0d4
Alexis Vasquez <36728288+alexismae@users.noreply.github.com>
2024-11-13 21:36:40 -0800

    Update script.js
    testing fetching

--- script.js
+++ script.js
@@ -3,32 +3,14 @@ let affirmations = [];
 const affirmationElement = document.getElementById('affirmation');
 
 function fetchAffirmations() {
-    const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTZO0oljRpa9rXw8KZGmv8KbDYEsi_6f8jIfiMzk7aCWS0fPxEPmiDSc3bONONYWFypdi204mmxNgdo/pub?gid=0&single=true&output=csv'; // Replace with your URL
-    
-    console.log('Fetching affirmations from:', sheetURL);
-    
-    fetch(sheetURL)
-        .then(response => {
-            console.log('Response received:', response);
-            return response.text();
-        })
-        .then(data => {
-            console.log('CSV data:', data);
-            affirmations = data.split('\n').map(row => row.trim());
-            console.log('Affirmations array:', affirmations);
-            showAffirmation();
-        })
-        .catch(error => {
-            console.error('Error fetching affirmations:', error);
-            affirmations = [
-                "You are capable of amazing things.",
-                "Every day is a new beginning.",
-                "Believe in yourself and all that you are.",
-                "Positivity is a choice you make.",
-                "You are stronger than you think."
-            ];
-            showAffirmation();
-        });
+    affirmations = [
+        "You are capable of amazing things.",
+        "Every day is a new beginning.",
+        "Believe in yourself and all that you are.",
+        "Positivity is a choice you make.",
+        "You are stronger than you think."
+    ];
+    showAffirmation();
 }
 
 function showAffirmation() {
